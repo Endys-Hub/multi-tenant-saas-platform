94199093db1968244bf44c59f306f4ceb7fbfa33
Ndudi <ndudiokehi@gmail.com>
2026-03-25 12:52:34 +0100

    JWT issue updated

--- apps/backend/src/plugins/jwt.ts
+++ apps/backend/src/plugins/jwt.ts
@@ -1,8 +1,11 @@
 import fp from "fastify-plugin";
 import jwt from "@fastify/jwt";
+import { FastifyPluginAsync } from "fastify";
 
-export default fp(async (app) => {
+const jwtPlugin: FastifyPluginAsync = async (app) => {
   await app.register(jwt as any, {
     secret: process.env.JWT_SECRET || "supersecret",
   });
-});
+};
+
+export default fp(jwtPlugin as any);
